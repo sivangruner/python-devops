eba025db0320d6b6afb50294e42f869465d5d82b
Sivan <Sivan.gruner@gmail.com>
2025-09-19 16:08:18 +0300

    upgraded textanalyzer moudle version  - changed build using toml file

---
+++ test_script/venv/Lib/site-packages/build/lib/TextAnalyzer/__init__.py
@@ -0,0 +1,14 @@
+from .core.analyzer import TextAnalyzer
+from .utils.text_ops import clean_text, extract_urls, fetch_web_content, remove_stopwords, extract_sentences
+from .config.settings import Config
+
+__version__ = "0.1.0"
+__all__ = [
+    "TextAnalyzer",
+    "clean_text",
+    "extract_urls",
+    "fetch_web_content",
+    "remove_stopwords",
+    "extract_sentences",
+    "Config"
+]
---
+++ test_script/venv/Lib/site-packages/build/lib/TextAnalyzer/utils/__init__.py
@@ -0,0 +1,3 @@
+from .text_ops import clean_text, extract_urls, fetch_web_content, remove_stopwords, extract_sentences
+
+__all__ = ["clean_text", "extract_urls", "fetch_web_content", "remove_stopwords", "extract_sentences"]
